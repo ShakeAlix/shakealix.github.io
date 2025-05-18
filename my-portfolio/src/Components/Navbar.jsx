0171e807610040cbc25fe501c30da5b963d62dad
ShakeAlix <26100051@lums.edu.pk>
2025-05-18 23:52:40 +0500

    .

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -2,9 +2,6 @@ import "../index.css"
 const Navbar = () => {
     return (
         <div className="w-full h-14 bg-slate-900 bg-opacity-10 shadow-lg fixed top-0 z-50 flex justify-between items-center backdrop-blur-sm">
-            <div className="flex items-center pl-6">
-                <img src="../../logo1.svg" className="opacity-50 h-6"/>
-            </div>
             
             <div className="absolute left-1/2 transform -translate-x-1/2">
                 <div className="flex gap-12 text-white text-lg font-josefin">
@@ -14,6 +11,10 @@ const Navbar = () => {
                     <a href="#experience" className="cursor-pointer hover:underline">experience</a>
                     <a href="#contact" className="cursor-pointer hover:underline">contact</a>
                 </div>
+            </div>
+            
+            <div className="absolute right-4 flex items-center px-1">
+                <img src="../../logo1.svg" className="opacity-50 h-6"/>
             </div>
         </div>
     )
